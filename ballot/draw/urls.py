cedf3b351a7b978cfd679e7f19730fdb80258f45
dcalsky <dcalsky@163.com>
2016-10-13 00:44:45 +0800

    init

---
+++ ballot/draw/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'submit$', views.submit)
+    # url(r'init$', views._init)
+]
---
+++ ballot/draw/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import User
+from django.http import HttpResponse
+from random import shuffle
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'draw/index.html')
+
+
+def submit(request):
+    name = request.POST.get('name')
+    user = User.objects.filter(name=name)
+    if name is '':
+        return render(request, 'draw/info.html', {'text': '姓名不能为空！'})
+    if user.exists():
+        return render(request, 'draw/info.html', {'text': '该名字已经存在！'})
+    else:
+        users = User.objects.filter(name='')
+        user = users[0]
+        user.name = name
+        user.save()
+        return render(request, 'draw/info.html', {'text': '您的编号是：' + str(user.number)})
+
+
+# def _init(request):
+#     array = []
+#     for i in range(1, 129):
+#         array.append(i)
+#     shuffle(array)
+#     for i in array:
+#         user = User(number=i)
+#         user.save()
+
