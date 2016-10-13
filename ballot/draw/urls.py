de5a857fba4b4d382e6a9e506a179d4401d8fa1b
dcalsky <dcalsky@163.com>
2016-10-13 10:45:08 +0800

    sqlite3

--- ballot/draw/urls.py
+++ ballot/draw/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'submit$', views.submit)
+    url(r'submit$', views.submit),
     url(r'init$', views._init)
 ]
