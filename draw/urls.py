dcb20cf5122340c00298e88cb37fb903ebb2435a
dcalsky <dcalsky@163.com>
2016-10-25 19:57:02 +0800

    update/dir: update structure; gitignore: remove unnecessary item

--- draw/urls.py
+++ draw/urls.py
@@ -1,10 +1,9 @@
-
 from django.conf.urls import url
+
 from . import views
 
-
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'submit$', views.submit),
     url(r'danger_init$', views.init)
 ]
